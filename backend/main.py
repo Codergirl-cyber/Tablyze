7d6b43aee4b5036bda48abda2d710dab8f5e4fa9
shruti <shrutibhute24@gmail.com>
2026-06-29 20:38:16 +0530

    fixed routing

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # Allow frontend (Next.js)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://tablyze.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
